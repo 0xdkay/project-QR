69b05646f5a73bb5e80b06fd20472a322810adbb
Dongkwan Kim <insinoa@gmail.com>
2012-04-14 02:11:00 +0900

    first commit

---
+++ gen/kr/dkay/qrcoderace/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kr.dkay.qrcoderace;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/kr/dkay/qrcoderace/QrcodeMain.java
@@ -0,0 +1,13 @@
+package kr.dkay.qrcoderace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QrcodeMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
