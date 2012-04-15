fc3f28102844b8725960405d60d10c4a2ab63f55
Dongkwan Kim <insinoa@gmail.com>
2012-04-15 18:02:27 +0900

    change layout to tabhost view and set gps setting

--- gen/kr/dkay/qrcoderace/R.java
+++ gen/kr/dkay/qrcoderace/R.java
@@ -18,14 +18,10 @@ public final class R {
     }
     public static final class id {
         public static final int about=0x7f060001;
-        public static final int aboutbutton=0x7f060005;
         public static final int remain=0x7f060002;
-        public static final int remainbutton=0x7f060006;
         public static final int setting=0x7f060004;
-        public static final int settingbutton=0x7f060008;
         public static final int solved=0x7f060003;
-        public static final int solvedbutton=0x7f060007;
-        public static final int worldMap=0x7f060000;
+        public static final int worldmap=0x7f060000;
     }
     public static final class layout {
         public static final int listview_layout_probs=0x7f030000;
--- src/kr/dkay/qrcoderace/QrcodeMain.java
+++ src/kr/dkay/qrcoderace/QrcodeMain.java
@@ -1,25 +1,33 @@
 package kr.dkay.qrcoderace;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import android.app.Activity;
+
 import android.app.AlertDialog;
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.widget.*;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.View;
+import android.view.*;
+import android.widget.*;
 
-public class QrcodeMain extends Activity {
+public class QrcodeMain extends TabActivity {
 	/** Called when the activity is first created. */
-	
-	private Button bAbout;
-	private Button bRemain;
-	private Button bSolved;
-	private Button bSetting;
 	
 	private ImageView worldMap;
 	private TextView about;
@@ -27,31 +35,55 @@ public class QrcodeMain extends Activity {
 	private ListView solved;
 	private ListView setting;
 	
-	
 	private boolean isTwoClickBack = false;
-	private char curVisible = 'w';
 	private ArrayList<String> remainList;
 	private ArrayList<String> solvedList;
+ 
+	private TabHost mTabHost;
+	
+	private Double myLat;
+	private Double myLng;
+	private String myTeam = "test";
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Resources res = getResources();
+
         
-        remainList = new ArrayList<String>();
-        solvedList = new ArrayList<String>();
+        //setting tabs
+        mTabHost = getTabHost();
+        mTabHost.addTab(mTabHost.newTabSpec("tab") 
+        		.setIndicator("worldmap") 
+        		.setContent(R.id.worldmap)); 
+        mTabHost.addTab(mTabHost.newTabSpec("tab") 
+        		.setIndicator("about") 
+        		.setContent(R.id.about)); 
+        mTabHost.addTab(mTabHost.newTabSpec("tab") 
+        		.setIndicator("remain") 
+                .setContent(R.id.remain)); 
+        mTabHost.addTab(mTabHost.newTabSpec("tab") 
+                .setIndicator("solved") 
+                .setContent(R.id.solved)); 
+        mTabHost.addTab(mTabHost.newTabSpec("tab") 
+                .setIndicator("setting") 
+                .setContent(R.id.setting)); 
+        mTabHost.setCurrentTab(0);
         
-        worldMap = (ImageView)findViewById(R.id.worldMap);
+        //setting variables related to each tab
+        worldMap = (ImageView)findViewById(R.id.worldmap);
         about = (TextView)findViewById(R.id.about);
         solved = (ListView)findViewById(R.id.solved);
         remain = (ListView)findViewById(R.id.remain);
         setting = (ListView)findViewById(R.id.setting);
         
-        bAbout = (Button)findViewById(R.id.aboutbutton);
-        bRemain = (Button)findViewById(R.id.remainbutton);
-        bSolved = (Button)findViewById(R.id.solvedbutton);
-        bSetting = (Button)findViewById(R.id.settingbutton);
         
+        
+        //setting contents in the tabs
+        remainList = new ArrayList<String>();
+        solvedList = new ArrayList<String>();
         
         remain.setAdapter(new ArrayAdapter <String> (this,
                 R.layout.listview_layout_probs,
@@ -60,105 +92,23 @@ public class QrcodeMain extends Activity {
                 R.layout.listview_layout_probs,
                 solvedList));
         
-        Resources res = getResources();
+        
         String settingList[] = res.getStringArray(R.array.test);        
         setting.setAdapter(new ArrayAdapter <String> (this,
                 R.layout.listview_layout_probs,
                 settingList));
-
-        Button.OnClickListener adapter = new Button.OnClickListener() {
+        
+        
+        HttpPostData();
+        ImageView.OnClickListener adapter = new ImageView.OnClickListener() {
+        	@Override
         	public void onClick(View v){
-        		switch(v.getId()){
-        		case R.id.aboutbutton:
-        			About();
-        			break;
-        		case R.id.remainbutton:
-        			Remain();
-        			break;
-        		case R.id.solvedbutton:
-        			Solved();
-        			break;
-        		case R.id.settingbutton:
-        			Setting();
-        			break;
-        		}
+        		HttpPostData();
         	}
         };
+        worldMap.setOnClickListener(adapter);
         
-        bAbout.setOnClickListener(adapter);
-        bRemain.setOnClickListener(adapter);
-        bSolved.setOnClickListener(adapter);
-        bSetting.setOnClickListener(adapter);
-    }
-/* 
-	AlertDialog.Builder ad = new AlertDialog.Builder(this);
-	ad.setTitle("Result");
-	ad.setCancelable(true);
-	ad.setPositiveButton("Close", null);
-	ad.setMessage(curVisible);
-	ad.show();
-*/
-    protected void About(){
-    	if(curVisible=='a')
-    		makeVisible('w');
-    	else
-    		makeVisible('a');
-    }
-    protected void Remain(){
-    	if(curVisible=='r')
-    		makeVisible('w');
-    	else
-    		makeVisible('r');
-    }
-    protected void Solved(){
-    	if(curVisible=='s')
-    		makeVisible('w');
-    	else
-    		makeVisible('s');
-    }
-    protected void Setting(){
-    	if(curVisible=='e')
-    		makeVisible('w');
-    	else
-    		makeVisible('e');
-    }
-    
-    void makeVisible(char token){
-    	if(curVisible == token) return;
-    	switch(curVisible){
-		case 'a':
-			this.about.setVisibility(View.INVISIBLE);
-			break;
-		case 'r':
-			this.remain.setVisibility(View.INVISIBLE);
-			break;
-		case 's':
-			this.solved.setVisibility(View.INVISIBLE);
-			break;
-		case 'e':
-			this.setting.setVisibility(View.INVISIBLE);
-			break;
-		case 'w':
-			this.worldMap.setVisibility(View.INVISIBLE);
-			break;
-		}
-    	switch(token){
-    	case 'a':
-    		this.about.setVisibility(View.VISIBLE);
-    		break;
-    	case 'r':
-    		this.remain.setVisibility(View.VISIBLE);
-    		break;
-    	case 's':
-    		this.solved.setVisibility(View.VISIBLE);
-    		break;
-    	case 'e':
-    		this.setting.setVisibility(View.VISIBLE);
-    		break;
-    	case 'w':
-    		this.worldMap.setVisibility(View.VISIBLE);
-    	}
-    	curVisible = token;
+        
     }
     
     @Override
@@ -166,8 +116,8 @@ public class QrcodeMain extends Activity {
     	if(event.getAction() == KeyEvent.ACTION_DOWN){
     		if(keyCode == KeyEvent.KEYCODE_BACK){
     			if(!isTwoClickBack){
-    				if(curVisible!='w'){
-    					makeVisible('w');		
+    				if(mTabHost.getCurrentTab()!=0){
+    					mTabHost.setCurrentTab(0);
     				}else{
     					Toast a = Toast.makeText(this, "Click 'close' button once more to exit." , Toast.LENGTH_SHORT);
     					a.setGravity(Gravity.CENTER, 0, 0);
@@ -182,7 +132,7 @@ public class QrcodeMain extends Activity {
     		}
     	}
     	return false;
-    }
+    }//onCreate
     
     class CntTimer extends CountDownTimer{
         public CntTimer(long millisInFuture, long countDownInterval) {
@@ -199,7 +149,124 @@ public class QrcodeMain extends Activity {
         	// TODO Auto-generated method stub
         	Log.i("Test"," isTwoClickBack " + isTwoClickBack);
         }
-    }
+    }//class CntTimer
+    
+    void HttpPostData() {
+    	
+    	if(!getMyLocation()){
+    		Toast.makeText(this, "No GPS Information", 0).show();
+    		return;
+    	}
+        try {
+             //--------------------------
+             //   URL 설정하고 접속하기
+             //--------------------------
+             URL url = new URL("http://drama.kaist.ac.kr/qr/getLocation.php");       // URL 설정
+             HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
+             //--------------------------
+             //   전송 모드 설정 - 기본적인 설정이다
+             //--------------------------
+             http.setDefaultUseCaches(false);                                           
+             http.setDoInput(true);                         // 서버에서 읽기 모드 지정
+             http.setDoOutput(true);                       // 서버로 쓰기 모드 지정 
+             http.setRequestMethod("POST");         // 전송 방식은 POST
+
+             // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+             http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+             //--------------------------
+             //   서버로 값 전송
+             //--------------------------
+             StringBuffer buffer = new StringBuffer();
+             buffer.append("lat").append("=").append(myLat).append("&");
+             buffer.append("long").append("=").append(myLng).append("&");
+             buffer.append("team").append("=").append(myTeam);
+             
+             OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
+             PrintWriter writer = new PrintWriter(outStream);
+             writer.write(buffer.toString());
+             writer.flush();
+             //--------------------------
+             //   서버에서 전송받기
+             //--------------------------
+             InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "EUC-KR"); 
+             BufferedReader reader = new BufferedReader(tmp);
+             StringBuilder builder = new StringBuilder();
+             String str;
+             while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
+                  builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
+             }
+             
+             String myResult = builder.toString();                       // 전송결과를 전역 변수에 저장
+             Toast.makeText(this, myResult, 0).show();
+        } catch (MalformedURLException e) {
+               //
+        } catch (IOException e) {
+               // 
+        } // try
+   } // HttpPostData
+    
+    
+    private boolean getMyLocation(){     
+    	LocationManager myLocationManager;
+    	LocationListener myLocationListener;
+    	//GPS가 켜져있는지 확인한다.
+    	myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    	Location loc = myLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    	if(loc == null)
+    	{
+    		Log.i("Worldmap", "GPS OFF");
+    		AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
+    		gsDialog.setTitle("GPS Status OFF");
+    		gsDialog.setMessage("Go to Change GPS Setting!");
+    		gsDialog.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    				// GPS설정 화면으로 튀어요
+    				Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+    				intent.addCategory(Intent.CATEGORY_DEFAULT);
+    				startActivity(intent);
+    			}
+    		});
+    		gsDialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    			}
+    		});
+    		gsDialog.create().show();
+    		return false;
+    	}
+
+    	myLocationListener = new LocationListener() {
+    		@Override
+    			public void onStatusChanged(String provider, int status, Bundle extras) {
+    			}
+    		@Override
+    			public void onProviderEnabled(String provider) {
+    			}
+    		@Override
+    			public void onProviderDisabled(String provider) {
+    			}
+    		//위치 정보가 변경 됐을 때 위치 정보를 가져온다.
+    		@Override
+    			public void onLocationChanged(Location location) {
+    				Log.i("HONEYMAP","Lat:"+location.getLatitude()+"Lng:"+location.getLongitude());
+    				myLat = location.getLatitude()*1E6;
+    				myLng = location.getLongitude()*1E6;
+    			}
+    	};
+
+    	//쥐피에스로 부터 위치 변경이 올 경우 업데이트 하도록 설정
+    	myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, myLocationListener);
+
+    	//가장 최근 위치를 저장한다.
+    	myLat = myLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude()*1E6;
+    	myLng = myLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude()*1E6;
+
+    	Log.i("HONEYMAP","LAT:"+myLat.intValue()+
+    			"Lng:"+myLng.intValue());
+
+    	return true;
+    }//get my location
+
+
 }
 
 
