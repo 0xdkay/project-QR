e5737fc4cc5ded3afd0b99d32d46d2af8e8c7a85
Dongkwan Kim <insinoa@gmail.com>
2012-04-15 14:59:12 +0900

    activate the buttons

--- gen/kr/dkay/qrcoderace/R.java
+++ gen/kr/dkay/qrcoderace/R.java
@@ -8,22 +8,31 @@
 package kr.dkay.qrcoderace;
 
 public final class R {
+    public static final class array {
+        public static final int test=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int aboutbutton=0x7f050001;
-        public static final int currentbutton=0x7f050002;
-        public static final int settingbutton=0x7f050004;
-        public static final int solvedbutton=0x7f050003;
-        public static final int worldMap=0x7f050000;
+        public static final int about=0x7f060001;
+        public static final int aboutbutton=0x7f060005;
+        public static final int remain=0x7f060002;
+        public static final int remainbutton=0x7f060006;
+        public static final int setting=0x7f060004;
+        public static final int settingbutton=0x7f060008;
+        public static final int solved=0x7f060003;
+        public static final int solvedbutton=0x7f060007;
+        public static final int worldMap=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int listview_layout_probs=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
+        public static final int about=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int desc=0x7f040002;
         public static final int hello=0x7f040000;
--- src/kr/dkay/qrcoderace/QrcodeMain.java
+++ src/kr/dkay/qrcoderace/QrcodeMain.java
@@ -1,13 +1,205 @@
 package kr.dkay.qrcoderace;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.*;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
 
 public class QrcodeMain extends Activity {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
+	
+	private Button bAbout;
+	private Button bRemain;
+	private Button bSolved;
+	private Button bSetting;
+	
+	private ImageView worldMap;
+	private TextView about;
+	private ListView remain;
+	private ListView solved;
+	private ListView setting;
+	
+	
+	private boolean isTwoClickBack = false;
+	private char curVisible = 'w';
+	private ArrayList<String> remainList;
+	private ArrayList<String> solvedList;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        remainList = new ArrayList<String>();
+        solvedList = new ArrayList<String>();
+        
+        worldMap = (ImageView)findViewById(R.id.worldMap);
+        about = (TextView)findViewById(R.id.about);
+        solved = (ListView)findViewById(R.id.solved);
+        remain = (ListView)findViewById(R.id.remain);
+        setting = (ListView)findViewById(R.id.setting);
+        
+        bAbout = (Button)findViewById(R.id.aboutbutton);
+        bRemain = (Button)findViewById(R.id.remainbutton);
+        bSolved = (Button)findViewById(R.id.solvedbutton);
+        bSetting = (Button)findViewById(R.id.settingbutton);
+        
+        
+        remain.setAdapter(new ArrayAdapter <String> (this,
+                R.layout.listview_layout_probs,
+                remainList));
+        solved.setAdapter(new ArrayAdapter <String> (this,
+                R.layout.listview_layout_probs,
+                solvedList));
+        
+        Resources res = getResources();
+        String settingList[] = res.getStringArray(R.array.test);        
+        setting.setAdapter(new ArrayAdapter <String> (this,
+                R.layout.listview_layout_probs,
+                settingList));
+
+        Button.OnClickListener adapter = new Button.OnClickListener() {
+        	public void onClick(View v){
+        		switch(v.getId()){
+        		case R.id.aboutbutton:
+        			About();
+        			break;
+        		case R.id.remainbutton:
+        			Remain();
+        			break;
+        		case R.id.solvedbutton:
+        			Solved();
+        			break;
+        		case R.id.settingbutton:
+        			Setting();
+        			break;
+        		}
+        	}
+        };
+        
+        bAbout.setOnClickListener(adapter);
+        bRemain.setOnClickListener(adapter);
+        bSolved.setOnClickListener(adapter);
+        bSetting.setOnClickListener(adapter);
+    }
+/* 
+	AlertDialog.Builder ad = new AlertDialog.Builder(this);
+	ad.setTitle("Result");
+	ad.setCancelable(true);
+	ad.setPositiveButton("Close", null);
+	ad.setMessage(curVisible);
+	ad.show();
+*/
+    protected void About(){
+    	if(curVisible=='a')
+    		makeVisible('w');
+    	else
+    		makeVisible('a');
+    }
+    protected void Remain(){
+    	if(curVisible=='r')
+    		makeVisible('w');
+    	else
+    		makeVisible('r');
+    }
+    protected void Solved(){
+    	if(curVisible=='s')
+    		makeVisible('w');
+    	else
+    		makeVisible('s');
+    }
+    protected void Setting(){
+    	if(curVisible=='e')
+    		makeVisible('w');
+    	else
+    		makeVisible('e');
+    }
+    
+    void makeVisible(char token){
+    	if(curVisible == token) return;
+    	switch(curVisible){
+		case 'a':
+			this.about.setVisibility(View.INVISIBLE);
+			break;
+		case 'r':
+			this.remain.setVisibility(View.INVISIBLE);
+			break;
+		case 's':
+			this.solved.setVisibility(View.INVISIBLE);
+			break;
+		case 'e':
+			this.setting.setVisibility(View.INVISIBLE);
+			break;
+		case 'w':
+			this.worldMap.setVisibility(View.INVISIBLE);
+			break;
+		}
+    	switch(token){
+    	case 'a':
+    		this.about.setVisibility(View.VISIBLE);
+    		break;
+    	case 'r':
+    		this.remain.setVisibility(View.VISIBLE);
+    		break;
+    	case 's':
+    		this.solved.setVisibility(View.VISIBLE);
+    		break;
+    	case 'e':
+    		this.setting.setVisibility(View.VISIBLE);
+    		break;
+    	case 'w':
+    		this.worldMap.setVisibility(View.VISIBLE);
+    	}
+    	curVisible = token;
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if(event.getAction() == KeyEvent.ACTION_DOWN){
+    		if(keyCode == KeyEvent.KEYCODE_BACK){
+    			if(!isTwoClickBack){
+    				if(curVisible!='w'){
+    					makeVisible('w');		
+    				}else{
+    					Toast a = Toast.makeText(this, "Click 'close' button once more to exit." , Toast.LENGTH_SHORT);
+    					a.setGravity(Gravity.CENTER, 0, 0);
+    					a.show();
+    					CntTimer timer = new CntTimer(2000, 1);
+    					timer.start();
+    				}
+    			}else{
+    				finish();
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
+    }
+    
+    class CntTimer extends CountDownTimer{
+        public CntTimer(long millisInFuture, long countDownInterval) {
+        	super(millisInFuture, countDownInterval);
+        	isTwoClickBack = true;
+        }
+        @Override
+        public void onFinish() {
+        	// TODO Auto-generated method stub
+        	isTwoClickBack = false;
+        }
+        @Override
+        public void onTick(long millisUntilFinished) {
+        	// TODO Auto-generated method stub
+        	Log.i("Test"," isTwoClickBack " + isTwoClickBack);
+        }
     }
 }
+
+
